7aa5ae00ba463a0a82f90a2e2bf8f95cfb4f70ed
avivzacks <avivzacks@gmail.com>
2024-10-08 18:07:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// let
+
+// const
+
+
+
+
+let total=document.getElementById('total')
+
+
+let amount=0
+
+
+ function take(){
+amount=amount-1
+total.innerHTML=amount
+
+}
+
+
+ function add(){
+    amount=amount+1
+    total.innerHTML=amount
+
+}
+
